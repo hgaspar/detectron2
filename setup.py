d41e95e5ce790773d7c40ed4906145dd6910cbed
Harris Gasparakis <harris.gasparakis@amd.com>
2020-05-20 18:03:07 +0000

    Use the new hipify feature (rocmsoftwareplatform pytorch PR 656), to hipify files in place

--- setup.py
+++ setup.py
@@ -56,15 +56,11 @@ def get_extensions():
             output_directory=this_dir,
             includes="/detectron2/layers/csrc/*",
             show_detailed=True,
-            is_pytorch_extension=True)
+            is_pytorch_extension=True, 
+            allow_same_dir=True)
 
-        source_cuda = glob.glob(path.join(extensions_dir, "**", 'hip', "*.hip")) + glob.glob(
-            path.join(extensions_dir, 'hip', "*.hip"))
-
-        shutil.copy("detectron2/layers/csrc/box_iou_rotated/box_iou_rotated_utils.h", 
-                    "detectron2/layers/csrc/box_iou_rotated/hip/box_iou_rotated_utils.h")
-        shutil.copy("detectron2/layers/csrc/deformable/deform_conv.h", 
-                    "detectron2/layers/csrc/deformable/hip/deform_conv.h")
+        source_cuda = glob.glob(path.join(extensions_dir, "**", "*.hip")) + glob.glob(
+            path.join(extensions_dir, "*.hip"))
 
     else:
         source_cuda = glob.glob(path.join(extensions_dir, "**", "*.cu")) + glob.glob(
